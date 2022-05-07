25ca5417b22f5c90eab02208a9279ee9e7353af8
Izta1013 <mer21003@byui.edu>
2022-05-06 17:00:08 -0700

    update

---
+++ js/javascript.js
@@ -0,0 +1,32 @@
+//Variables
+const daynames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
+const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
+const d = new Date();
+const dayName = daynames[d.getDay()];
+const monthName = months[d.getMonth()];
+const year = d.getFullYear();
+const fulldate = '${dayname}, ${d.getDate} ${monthName} ${year}';
+
+//Output
+document.getElementById("currentdate").textContent = fulldate;
